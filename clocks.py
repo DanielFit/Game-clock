8702088177c59a79151e3492a204367e2782e600
Daniel Fitzgerald <104741488+DanielFit@users.noreply.github.com>
2022-05-20 15:25:09 +1000

    Update clocks.py

--- clocks.py
+++ clocks.py
@@ -1,6 +1,11 @@
 from random import randint
 
-
+"""This input loop is to assist running table top role playing games that use "clocks" as a mechanic. 
+Each time a player takes an action depeding if it is significantly aggressive or meticulous it may increase the segments of a clock. At a random interavl before 
+the gameover limit is reached a complication may be introdued. Ideally the complication would appear once and then disapear after further inputs, but I can't figure
+out how. This wouldbetter represent the flow of the game were after a certain number of segments has been filled in ie: 5 or more heat segments the guards would appear at 
+the players location. The players would deal with the issue and then the guards would no longer present an issue even though players may add further segments to the heat
+clock (until the game over limit."""
 
 heat = 0
 time = 0
@@ -8,8 +13,9 @@ time = 0
 timecomp = randint(2,9)
 heatcomp = randint(2,9)
 
+gameover = 10
 
-while heat <= 10 and time <= 10:
+while heat <= gameover and time <= gameover:
  clock = input("enter clock time or heat \n")
  if clock == "heat":
      segment = input("enter segments \n")
@@ -26,7 +32,7 @@ while heat <= 10 and time <= 10:
      print ("heat = ",heat, "time = ",time)
      while time >= timecomp:
          print("Times up!")
-if heat or time >= 10:
+if heat or time >= gameover:
     print("the resistance has been compromised! run for your lives!")
 
 
